c7abea6359f0e040fbbd6d667658c90c9e4a20ac
Zack Walsh <zachary.walsh@opusivs.com>
2022-03-10 12:13:40 -0500

    Auto stash before rebase of "ZackWalsh_DEV"

---
+++ OBSSwitcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OBSSwitcher")]
+[assembly: AssemblyDescription("Switch OBS Scenes With Ease! Made For Ultrawides where streaming is normally impossible.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MEAT Inc")]
+[assembly: AssemblyProduct("OBSSwitcher")]
+[assembly: AssemblyCopyright("Copyright © MEAT Inc - 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21117f97-8930-47c3-8d34-62b205f5df12")]
+
+// Version information
+[assembly: AssemblyVersion("1.4.2.17")]
+[assembly: AssemblyFileVersion("1.4.2.17")]
+
